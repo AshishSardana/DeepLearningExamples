e9c301065a861d4721220a85cc533a0bcd5d8721
Ashish <ashishsardana21@gmail.com>
2020-11-17 19:13:22 +0530

    add scripts for rn50

---
+++ TensorFlow/Classification/ConvNets/resnet50v1.5/triton/scripts/launch_server.sh
@@ -0,0 +1,15 @@
+NV_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-"1"}
+DETACHED=${DETACHED:-"-d"}
+
+# Start TRITON server in DETACHED state
+docker run --runtime=nvidia --rm $DETACHED \
+   --shm-size=1g \
+   --ulimit memlock=-1 \
+   --ulimit stack=67108864 \
+   -p8000:8000 \
+   -p8001:8001 \
+   -p8002:8002 \
+   --name triton_server_cont \
+   -e NVIDIA_VISIBLE_DEVICES=$NV_VISIBLE_DEVICES \
+   -v $PWD/triton/model/saved_model/nvidia_rn50_tf_amp/:/models/resnet50/1/model.savedmodel/ \
+   nvcr.io/nvidia/tritonserver:20.09-py3 tritonserver --model-store=/models --strict-model-config=false --log-verbose=1
---
+++ TensorFlow/Classification/ConvNets/resnet50v1.5/triton/scripts/launch_server_tfs.sh
@@ -0,0 +1,26 @@
+NV_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-"all"}
+DETACHED=${DETACHED:-"-it"}
+
+BATCHING=${1:-false}
+ARGUMENTS=""
+
+if [[ $BATCHING == true ]]; then
+  # enable batching with a specific batch_size and num_threads
+  #ARGUMENTS="--enable_batching --batching_parameters_file=$PWD/triton/batching_params_file_tfs"
+  # only enable batching with no specific params
+  ARGUMENTS="--enable_batching"
+fi
+
+echo "Using args:  $(echo "$ARGUMENTS" | sed -e 's/   -/\n-/g')"
+
+# Start TFS in DETACHED state
+docker run --gpus $NV_VISIBLE_DEVICES --rm $DETACHED \
+   --shm-size=1g \
+   --ulimit memlock=-1 \
+   --ulimit stack=67108864 \
+   -e MODEL_NAME=resnet50 \
+   -p8500:8500 \
+   -p8501:8501 \
+   -p8502:8502 \
+   -v $PWD/triton/model/saved_model/nvidia_rn50_tf_amp/:/models/resnet50/1/ \
+   tensorflow/serving:latest-gpu $ARGS
