864a68640772e2a618615d6287a9deb78a40ce17
Ashish <ashishsardana21@gmail.com>
2020-12-15 19:15:21 +0530

    scripts to profile resnet50 with nsys in triton/tfs

---
+++ TensorFlow/Classification/ConvNets/resnet50v1.5/triton/scripts/launch_server_nvtx.sh
@@ -0,0 +1,21 @@
+NV_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-"1"}
+DETACHED=${DETACHED:-"-d"}
+OUTPUT=${OUTPUT:-"try"}
+DURATION=${DURATION:-30}
+
+echo "$DURATION"
+
+# Start TRITON server in DETACHED state
+docker run --runtime=nvidia --rm $DETACHED \
+   --shm-size=1g \
+   --ulimit memlock=-1 \
+   --ulimit stack=67108864 \
+   -p8000:8000 \
+   -p8001:8001 \
+   -p8002:8002 \
+   --name triton_server_cont \
+   -e NVIDIA_VISIBLE_DEVICES=$NV_VISIBLE_DEVICES \
+   -v $PWD/triton/inference/:/models/ \
+   -v $PWD/nsys:/nsys \
+   gitlab-master.nvidia.com:5005/dl/dgx/tritonserver:instrumented-triton-devel nsys profile -t cuda,nvtx,cudnn --delay=10 --duration=$DURATION --force-overwrite true --output /nsys/$OUTPUT tritonserver --model-store=/models  --log-verbose=1 --backend-config=tensorflow,version=2
+   #gitlab-master.nvidia.com:5005/dl/dgx/tritonserver:instrumented-triton-devel tritonserver --model-store=/models --strict-model-config=false --log-verbose=1 --backend-config=tensorflow,version=2
